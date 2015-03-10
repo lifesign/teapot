2f527a7a529c4272d1c445ec507500857ab22f0b
lifesign <leocloudy@gmail.com>
2015-03-10 23:57:14 +0800

    add teapot yaml file

--- src/teapot/loader.php
+++ src/teapot/loader.php
@@ -1,24 +1,54 @@
 <?php namespace Teapot;
 
 use Symfony\Component\Console\Application;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Parser;
 
 class Loader
 {
+    const FILENAME = 'teapot.yml';
+
+    /**
+     * @var Application
+     */
     protected $app;
+
+    /**
+     * @var Commands
+     */
+    protected $commands;
+
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * @var Parser
+     */
+    private $yamlParser;
+
 
     /**
      * @param Application $app
      */
     public function __construct(Application $app) {
         $this->app = $app;
+        $this->finder = new Finder;
+        $this->filesystem = new Filesystem;
+        $this->yamlParser = new Parser;
     }
 
     /**
      * Add avaliable commands
      */
-    public function loadCommands() {
-        $this->app->add(new Commands\Project\ProjectInitCommand());
-        /**/
+    public function addCommands() {
+        foreach ((array) $this->commands as $command) {
+            $class = __NAMESPACE__ . "\\Commands\\".$command;
+            $this->app->add(new $class);
+        }
     }
 
     /**
@@ -26,7 +56,63 @@ class Loader
      * @return mixed
      */
     public function run() {
-        $this->loadCommands();
+        $this->resolveCommands();
+        $this->addCommands();
         $this->app->run();
+    }
+
+    private function resolveCommands() {
+        $filename = __DIR__ .'/../../'.self::FILENAME;
+
+        if (! $this->filesystem->exists($filename)) {
+            throw new Exception("No teapot.yml configuration file found");
+        }
+
+        $metadata = $this->parseYamlFile($filename);
+
+        return $this->commands = $this->getAvaiableCommand($metadata);
+    }
+
+    private function parseYamlFile($filename) {
+
+        try {
+            $config = $this->yamlParser->parse(file_get_contents($filename));
+        } catch (ParseException $e) {
+            throw new Exception("Parse Error");
+        }
+
+        return is_array($config) ? $config : array();
+    }
+
+    private function getAvaiableCommand($metadata = []) {
+        $commands = [];
+
+        $finder = new Finder();
+        $directories = $finder->directories()
+                              ->in(__DIR__ .'/commands')
+                              ->ignoreDotFiles(true)
+                              ->depth(0);
+        if (isset($metadata['exclude']['folder'])) {
+            $directories->exclude($metadata['exclude']['folder']);
+        }
+
+        foreach ($directories as $dir) {
+            $files = $finder->files()
+                            ->ignoreDotFiles(true);
+
+            //add exclue files
+            if (isset($metadata['exclude']['files'])) {
+                foreach ($metadata['exclude']['files'] as $file) {
+                    $files->notName($file . '.php');
+                }
+            }
+
+            $files->in($dir->getRealPath());
+            foreach ($files as $file) {
+                $commands[] = $dir->getRelativePathname() . '\\' . $file->getBasename('.php');
+            }
+        }
+
+        return $commands;
     }
 }
